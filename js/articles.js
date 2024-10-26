38eb1559da53c3bcc697b90df426a51748fe666e
Ensarrs <maliqii.ensar@gmail.com>
2024-10-26 19:54:28 +0200

    article.js changes

--- js/articles.js
+++ js/articles.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const apiKey = '8176d95dfa2d495387c2cfc16df59d3c';
-    const url = `https://newsapi.org/v2/top-headlines?category=technology&apiKey=${apiKey}`;
+    const apiKey = 'pub_57408c7c7feea6f53fcbc1bdce374c8784dda';
+    const url = `https://newsdata.io/api/1/news?apikey=${apiKey}&language=en&category=technology`;
     const newsContainer = document.getElementById("api-feed");
 
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            const items = data.articles;
+            const items = data.results;
             items.forEach(item => {
                 const title = item.title;
                 const description = item.description;
-                const link = item.url;
-                const pubDate = item.publishedAt;
-                const imageUrl = item.urlToImage;
+                const link = item.link;
+                const pubDate = item.pubDate;
+                const imageUrl = item.image_url;
                 const date = new Date(pubDate).toLocaleDateString(undefined, {
                     year: 'numeric',
                     month: 'long',
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 });
 
                 const articleCard = `
-                    <div class="api-card bg-white p-4 rounded-lg shadow">
+                    <div class="api-card p-4 rounded-lg shadow">
                         <div class="api-content">
                             <h3 class="api-h3 text-xl font-semibold mb-2">${title}</h3>
                             <p class="api-date text-gray-500">Released on: ${date}</p>
